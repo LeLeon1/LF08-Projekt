9b3171dbfebd694e50a7e6e0b6876ccf4000d695
TheNerdworld <112518111+TheNerdworld@users.noreply.github.com>
2024-01-08 19:27:51 +0100

    Insert von Log unfertig
    Das ist alles was ich zum Insert habe sorry nochmal dafür dasses nich fertig ist aber muss echt noch bisschen lernen (lange version in Audio auf Whats app)

---
+++ InsertData/InsertData/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data.SqlClient;
+class loadData
+{
+    static void Main()
+    {
+
+    }
+    static void loadFromLog()
+    {
+
+    }
+
+    static void connectDb()
+    {
+
+    }
+}
