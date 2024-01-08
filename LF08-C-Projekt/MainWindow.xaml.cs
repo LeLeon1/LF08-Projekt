6ff9f5711c9f98601f38829cf45b0f1d85c7981c
Mikudayo <mikudayojp@gmail.com>
2024-01-08 23:13:08 +0100

    added SQLite NuGet extension

--- LF08-C-Projekt/MainWindow.xaml.cs
+++ LF08-C-Projekt/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SQLite;
 
 namespace LF08_C_Projekt
 {
