dc8d9c00a7ec93e5f5977a03f2213952df347054
Mikudayo <mikudayojp@gmail.com>
2024-01-09 01:58:23 +0100

    #02 Datenbank via WPF
    somehow DB is viewable via UI

--- LF08-C-Projekt/MainWindow.xaml.cs
+++ LF08-C-Projekt/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Data.SQLite;
+using System.Data;
 
 namespace LF08_C_Projekt
 {
@@ -22,11 +11,19 @@ namespace LF08_C_Projekt
     /// </summary>
     public partial class MainWindow : Window
     {
+        SQLiteConnection conn = new SQLiteConnection(@"Data Source=C:\Users\leon\Documents\Neuer Ordner (3)\LF08-C-Projekt\LF08-C-Projekt\databases\logDb.db");
+        private System.Data.DataSet dataSet;
         public MainWindow()
         {
             InitializeComponent();
-
             //testComment
+            /*
+             MakeParentTable();
+             MakeChildTable();
+             MakeDataRelation();
+             BindToDataGrid();
+            */
+            FillDataGrid();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -36,6 +33,8 @@ namespace LF08_C_Projekt
             GetValue(ponyImage.VisibilityProperty);
             ponyImage.SetValue(VisibilityProperty, );
             */
+
+            //commandIP();
         }
 
         private void testie() {
@@ -47,6 +46,158 @@ namespace LF08_C_Projekt
 
            // DataGrid.ItemsSource = users;
 
+        }
+
+        private void commandTest()
+        {
+            conn.Open();
+
+            SQLiteCommand command = new SQLiteCommand("Select * from logs", conn);
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            //deliverToUser(reader);
+
+            reader.Close();
+        }
+
+        private void commandIP(String stringer)
+        {
+            conn.Open();
+
+            SQLiteCommand command = new SQLiteCommand("Select * from logs", conn);
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            //deliverToUserColumns(reader, "WHERE ip_adressen = '" + stringer + "'");
+
+            deliverToUserRow(reader);
+
+            reader.Close();
+        }
+
+        private void commandMethods()
+        {
+            conn.Open();
+
+            SQLiteCommand command = new SQLiteCommand("Select * from logs", conn);
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            //deliverToUser(reader);
+
+            reader.Close();
+        }
+
+        private void commandErrors()
+        {
+            conn.Open();
+
+            SQLiteCommand command = new SQLiteCommand("Select * from logs", conn);
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            //deliverToUser(reader);
+
+            reader.Close();
+        }
+
+        private void commandTimespan()
+        {
+            conn.Open();
+
+            SQLiteCommand command = new SQLiteCommand("Select * from logs", conn);
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            //deliverToUser(reader);
+
+            reader.Close();
+        }
+
+
+        //TODO - Change name of String
+        private void deliverToUserColumns(SQLiteDataReader reader, String smth)
+        {
+            while (reader.Read())
+                Console.WriteLine(reader[smth]);
+
+        }
+        
+        //TODO - Change name of String
+        private void deliverToUserRow(SQLiteDataReader reader)
+        {
+            var test = reader.GetValues();
+
+            Console.WriteLine();
+
+        }
+
+        private void deliverToUserTest(SQLiteCommand command)
+        {
+            /*
+            DataTable dt = new DataTable("YourTable");
+            SQLiteDataAdapter sda = new SQLiteDataAdapter(command);
+            sda.Fill(dt);
+            testDatagrid.ItemsSource = dt.DefaultView;
+            */
+
+            /*
+            // Create a new DataTable.
+            System.Data.DataTable table = new DataTable("ParentTable");
+            // Declare variables for DataColumn and DataRow objects.
+            DataColumn column;
+            DataRow row;
+
+            // Create new DataColumn, set DataType,
+            // ColumnName and add to DataTable.
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.ColumnName = "id";
+            column.ReadOnly = true;
+            column.Unique = true;
+            // Add the Column to the DataColumnCollection.
+            table.Columns.Add(column);
+
+            // Create second column.
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "ParentItem";
+            column.AutoIncrement = false;
+            column.Caption = "ParentItem";
+            column.ReadOnly = false;
+            column.Unique = false;
+            // Add the column to the table.
+            table.Columns.Add(column);
+
+            // Make the ID column the primary key column.
+            DataColumn[] PrimaryKeyColumns = new DataColumn[1];
+            PrimaryKeyColumns[0] = table.Columns["id"];
+            table.PrimaryKey = PrimaryKeyColumns;
+
+            // Instantiate the DataSet variable.
+            dataSet = new DataSet();
+            // Add the new DataTable to the DataSet.
+            dataSet.Tables.Add(table);
+
+            testDatagrid.SetDataBinding(dataSet, "ParentTable");
+            */
+
+           
+        }
+        private void FillDataGrid()
+        {
+
+            conn.Open();
+            SQLiteCommand cmd = new SQLiteCommand("select * from logs", conn);
+            SQLiteDataAdapter sda = new SQLiteDataAdapter(cmd);
+            //Database1DataSet ds = new Database1DataSet();
+            
+            DataTable dt = new DataTable("verstehe ich nicht");
+            sda.Fill(dt);
+            dataGrid1.ItemsSource = dt.DefaultView;
+            /*
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                dataGrid1.ItemsSource = ds.Tables[0].DefaultView;
+            }
+            */
+            conn.Close();
         }
     }
 }
